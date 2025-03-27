5c3bd304c52d84d45152987ea58e860b03b3ea95
JungAh <70677614+Jungahgo@users.noreply.github.com>
2025-03-28 02:12:25 +0900

    init

---
+++ src/main/java/com/koa/RingDong/RingDongApplication.java
@@ -0,0 +1,13 @@
+package com.koa.RingDong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingDongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RingDongApplication.class, args);
+	}
+
+}
