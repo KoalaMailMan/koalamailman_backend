1eac76f56234691d6b2a07121f98633ffb6c321a
JungAh <70677614+Jungahgo@users.noreply.github.com>
2025-03-28 02:12:25 +0900

    init

---
+++ src/test/java/com/koa/RingDong/RingDongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koa.RingDong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RingDongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
