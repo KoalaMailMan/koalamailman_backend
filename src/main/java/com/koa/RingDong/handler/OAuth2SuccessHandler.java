bc7af7b63e2bb5c34bc9198f53c493c939218d39
JungAh <70677614+Jungahgo@users.noreply.github.com>
2025-06-09 23:22:16 +0900

    feat: 카카오 OAuth2 로그인 연동 추가

--- src/main/java/com/koa/RingDong/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/koa/RingDong/handler/OAuth2SuccessHandler.java
@@ -1,5 +1,6 @@
 package com.koa.RingDong.handler;
 
+import com.koa.RingDong.entity.OAuthProvider;
 import com.koa.RingDong.entity.User;
 import com.koa.RingDong.provider.TokenProvider;
 import com.koa.RingDong.repository.UserRepository;
@@ -8,6 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -27,21 +29,26 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException {
 
-
-        // 인증된 사용자 정보에서 id 추출
+        // 1. 인증된 사용자 정보에서 attribute 추출
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-        String kakaoId = oAuth2User.getAttribute("id").toString();
-        User user = userRepository.findByKakaoId(kakaoId)
-                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
+        String registrationId = ((OAuth2AuthenticationToken) authentication).getAuthorizedClientRegistrationId();
 
-        // JWT access token 생성
+        // 2. oauthId 추출
+        String oauthId = oAuth2User.getAttribute("id").toString();
+
+        // 3. DB에서 유저 조회
+        User user = userRepository.findByOauthIdAndOauthProvider(
+                oauthId,
+                OAuthProvider.valueOf(registrationId.toUpperCase())
+        ).orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
+
+        // 4. JWT access token 생성
         String jwtAccessToken = tokenProvider.generateAccessToken(user.getId());
-        // (필요시 refreshToken도 생성)
-
         log.info("[JWT 생성] userId: {}, token: {}", user.getId(), jwtAccessToken);
 
-        // access token 프론트에 응답
+        // 5. acces token 프론트에 응답
         response.setHeader("Authorization", "Bearer " + jwtAccessToken);
         response.setStatus(HttpServletResponse.SC_OK);
     }
 }
+
