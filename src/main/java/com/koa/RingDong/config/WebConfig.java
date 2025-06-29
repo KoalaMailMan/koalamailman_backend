3995d2d69ded9488786d3825a7a7dcfc6b6c813a
JungAh <70677614+Jungahgo@users.noreply.github.com>
2025-06-29 14:18:57 +0900

    fix: CORS 설정 추가

---
+++ src/main/java/com/koa/RingDong/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.koa.RingDong.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${app.oauth2.front-uri}")
+    private String frontUri;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(frontUri)
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
