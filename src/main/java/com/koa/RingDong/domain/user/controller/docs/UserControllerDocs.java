47c85f1dbe4a0b29460adb4b237c21d1ab08e970
JungAh <70677614+Jungahgo@users.noreply.github.com>
2025-08-26 01:54:52 +0900

    feat: User 예외 처리 추가 및 Swagger 문서화

---
+++ src/main/java/com/koa/RingDong/domain/user/controller/docs/UserControllerDocs.java
@@ -0,0 +1,44 @@
+package com.koa.RingDong.domain.user.controller.docs;
+
+import com.koa.RingDong.domain.user.dto.UpdateUserProfileRequest;
+import com.koa.RingDong.domain.user.dto.UserResponse;
+import com.koa.RingDong.global.dto.ErrorResponse;
+import com.koa.RingDong.global.dto.SuccessResponse;
+import com.koa.RingDong.global.security.oauth.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+
+@Tag(name = "사용자", description = "사용자 관련 API입니다.")
+public interface UserControllerDocs {
+    @Operation(summary = "유저 정보 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "유저 정보 조회 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "유저를 찾을 수 없음",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+    })
+    SuccessResponse<UserResponse> getUserInfo(
+            @Parameter(hidden = true)
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    );
+
+    @Operation(summary = "유저 프로필 수정")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "유저 프로필 수정 성공"),
+            @ApiResponse(responseCode = "404", description = "유저를 찾을 수 없음",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+    })
+    void updateUserProfile(
+            @Parameter(hidden = true)
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            final UpdateUserProfileRequest request
+    );
+}
