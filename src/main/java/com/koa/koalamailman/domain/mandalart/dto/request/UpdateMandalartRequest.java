8839a854c1c363ec2b75170ef2f1fd42fb03a2f2
JungAh <70677614+Jungahgo@users.noreply.github.com>
2025-09-18 02:16:44 +0900

    [#9] fix: reminderOption enum처리, Valid 확인

--- src/main/java/com/koa/koalamailman/domain/mandalart/dto/request/UpdateMandalartRequest.java
+++ src/main/java/com/koa/koalamailman/domain/mandalart/dto/request/UpdateMandalartRequest.java
@@ -1,10 +1,12 @@
 package com.koa.koalamailman.domain.mandalart.dto.request;
 
 import com.koa.koalamailman.domain.mandalart.repository.entity.ReminderOption;
+import jakarta.validation.Valid;
 
 public record UpdateMandalartRequest(
         Long mandalartId,
         ReminderOption reminderOption,
+        @Valid
         UpdateCoreGoalRequest core
 ) {
 }
--- src/main/java/com/koa/koalamailman/domain/mandalart/repository/entity/MandalartEntity.java
+++ src/main/java/com/koa/koalamailman/domain/mandalart/repository/entity/MandalartEntity.java
@@ -27,6 +27,8 @@ public class MandalartEntity extends BaseEntity {
     @Column(name = "user_id", nullable = false)
     private Long userId;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "remind_interval", nullable = false, length = 15)
     @Embedded
     private ReminderOption reminderOption;
 
