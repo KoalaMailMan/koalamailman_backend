711f39ead4714e1977309f9d31c8cb3007e9906f
JungAh <70677614+Jungahgo@users.noreply.github.com>
2025-09-30 00:07:33 +0900

    [#69]refactor: RequestDataWrapper 내부 @Valid처리 누락

--- src/main/java/com/koa/koalamailman/global/dto/RequestDataWrapper.java
+++ src/main/java/com/koa/koalamailman/global/dto/RequestDataWrapper.java
@@ -1,8 +1,12 @@
 package com.koa.koalamailman.global.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class RequestDataWrapper<T> {
+    @Valid
+    @NotNull
     private T data;
 }
