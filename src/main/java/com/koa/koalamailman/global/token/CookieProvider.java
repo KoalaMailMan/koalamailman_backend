2dfa07a9ed5343dd74940e72cb0c23d380daa65e
JungAh <70677614+Jungahgo@users.noreply.github.com>
2026-05-18 22:02:08 +0900

    fix: CookieProvider에 쿠키 domain 속성 적용

--- src/main/java/com/koa/koalamailman/global/token/CookieProvider.java
+++ src/main/java/com/koa/koalamailman/global/token/CookieProvider.java
@@ -11,6 +11,9 @@ import java.time.Duration;
 public class CookieProvider {
     @Value("${jwt.refresh.expiration}")
     private long refreshTokenExpiration;
+    @Value("${app.oauth2.domain}")
+    private String cookieDomain;
+
     public ResponseCookie setRefreshTokenCookie(String refreshToken) {
         return ResponseCookie.from("refresh_token", refreshToken)
                 .httpOnly(true)
@@ -18,6 +21,7 @@ public class CookieProvider {
                 .path("/")
                 .maxAge(Duration.ofMillis(refreshTokenExpiration))
                 .sameSite("Lax")
+                .domain(cookieDomain)
                 .build();
     }
 }
